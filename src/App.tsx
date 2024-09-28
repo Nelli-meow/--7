798bc9d0d3377fd5c7250b6d3b982a53f8e94b48
Nelli Gorlova <nelli7035@gmail.com>
2024-09-28 13:08:48 +0600

    added a function for adding a new item

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,95 @@
 import { useState } from 'react'
-
 import './App.css'
+import Items from "./components/Items/Items.tsx";
+import drinkImage from '../../assets/free-icon-drinks-665885.png';
+import foodImage from '../../assets/free-icon-dinner-272186.png';
+import * as React from "react";
+import AddedItems from "./components/AddedItems/AddedItems.tsx";
 
-const App = () => {
-  const [count, setCount] = useState(0)
+interface Props {
+    drinkImage:  string;
+    foodImage:  string;
+}
+
+const App: React.FC<Props> = ({drinkImage,foodImage}) => {
+    const [addedItems, setAddedItems] = useState([]);
+
+    const itemsFood  = [
+        {name: 'Hamburger', price: 80, image: foodImage},
+        {name: 'Cheeseburger', price: 90,image: foodImage},
+        {name: 'Fries', price: 45,image: foodImage},
+    ];
+
+    const itemsDrinks = [
+        {name: 'Coffee', price: 70,image: drinkImage},
+        {name: 'Tea', price: 50,image: drinkImage},
+        {name: 'Cola', price: 40,image: drinkImage},
+    ];
+
+
+    const addItem = (item: { name: string, price: number }) => {
+        let itemExists = false;
+
+        const updatedItems = addedItems.map((addedItem) => {
+            if (addedItem.name === item.name) {
+                itemExists = true;
+                return { ...addedItem, amount: addedItem.amount += 1 };
+            }
+            return addedItem;
+        });
+
+        if (!itemExists) {
+            setAddedItems([...addedItems, { name: item.name, price: item.price, amount: 1 }]);
+        } else {
+            setAddedItems(updatedItems);
+        }
+    };
+
+    const totalPrice = addedItems.reduce((acc, item) => acc + item.price * item.amount, 0);
 
   return (
     <>
-      <div>
-
+      <div className="App-container">
+          <div className="order-details">
+              {addedItems.length === 0 ? (
+                  <span>Order is empty! Please add some items!</span>
+              ) : (
+                  <>
+                      {addedItems.map((item, index) => (
+                          <AddedItems
+                              key={index}
+                              itemName={item.name}
+                              itemPrice={item.price}
+                              itemAmount={item.amount}
+                              totalPrice={totalPrice}
+                          />
+                      ))}
+                      <h3>Total Price: {totalPrice} KGS</h3>
+                  </>
+              )}
+          </div>
+          <div className="add-items">
+              <div className="food-items">
+                  {itemsFood.map((item, index) => (
+                      <Items
+                          key={index}
+                          addItem={() => addItem(item)}
+                          itemName={item.name}
+                          itemPrice={item.price}
+                          itemImage={foodImage}/>
+                  ))}
+              </div>
+              <div className="drinks-items">
+                  {itemsDrinks.map((item, index) => (
+                      <Items
+                          key={index}
+                          addItem={() => addItem(item)}
+                          itemName={item.name}
+                          itemPrice={item.price}
+                          itemImage={foodImage}/>
+                  ))}
+              </div>
+          </div>
       </div>
     </>
   )
--- src/components/AddedItems/AddedItems.tsx
+++ src/components/AddedItems/AddedItems.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
+import './AddedItems.css';
 
 interface Props {
-    itemName: string;
-    itemPrice: number;
-    itemAmount: number;
-    totalPrice: number;
+    itemName: string,
+    itemPrice: number,
+    itemAmount: number,
+    totalPrice: number,
+    key?: number
 }
 
-const AddedItems: React.FC<Props> = ({itemName, itemPrice, totalPrice,itemAmount}) => {
+const AddedItems: React.FC<Props> = ({itemName, itemPrice, itemAmount, key}) => {
     return (
-        <div>
+        <div key={key} className="added-item">
             <h3 className="item-title">{itemName}</h3>
-            <span className="item-ammount">x: {itemAmount}</span>
+            <span className="item-ammount">× {itemAmount}</span>
             <span className="item-price">{itemPrice} KGS</span>
         </div>
     );
